a4efc1b76c0c41b7de55acca18aa95a92c36a2cb
ErikV121 <ErikVasquez121@gmail.com>
2025-03-12 17:20:54 -0400

    Created findByUrl

--- src/main/java/com/erikv121/blogapp/repository/PostRepository.java
+++ src/main/java/com/erikv121/blogapp/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.erikv121.blogapp.repository;
 import com.erikv121.blogapp.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PostRepository extends JpaRepository<Post, UUID> {
+    Optional<Post> findByUrl(String url);
 }
