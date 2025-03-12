9a1eddc5cb92285466f9c278cc8d85fc6842757b
ErikV121 <ErikVasquez121@gmail.com>
2025-03-12 17:20:20 -0400

    Updated mapping

--- src/main/java/com/erikv121/blogapp/mapper/PostMapper.java
+++ src/main/java/com/erikv121/blogapp/mapper/PostMapper.java
@@ -4,10 +4,19 @@ import com.erikv121.blogapp.dto.request.PostRequest;
 import com.erikv121.blogapp.dto.response.PostResponse;
 import com.erikv121.blogapp.entity.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
-    Post dtoToEntity(PostRequest postRequest);
-    
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "createdAt", source = "createdAt")
+    @Mapping(target = "author", expression = "java(post.isAnonymous() ? \"Anonymous\" : post.getOriginalAuthor())")
+    @Mapping(target = "url", source = "url")
     PostResponse entityToDto(Post post);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "anonymous", source = "anonymous")
+    @Mapping(target = "url", source = "url")
+    Post dtoToEntity(PostRequest postRequest);
 }
+
