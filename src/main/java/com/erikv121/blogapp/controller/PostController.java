27d87190e0adaed0611c398bdfd2409d8297e64a
ErikV121 <ErikVasquez121@gmail.com>
2025-03-12 17:17:41 -0400

    Updated and Created CRUD endpoints

--- src/main/java/com/erikv121/blogapp/controller/PostController.java
+++ src/main/java/com/erikv121/blogapp/controller/PostController.java
@@ -1,30 +1,81 @@
 package com.erikv121.blogapp.controller;
 
 import com.erikv121.blogapp.dto.request.PostRequest;
+import com.erikv121.blogapp.dto.response.PostResponse;
 import com.erikv121.blogapp.service.PostService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import java.util.UUID;
 
 @Controller
+@RequestMapping("/blog")
 public class PostController {
-    private PostService postService;
+    private final PostService postService;
 
     public PostController(PostService postService) {
         this.postService = postService;
     }
 
     @PostMapping("/posts")
-    public void addPost(@Valid @RequestBody PostRequest postRequest) {
+    public String createPost(@Valid @ModelAttribute("post") PostRequest postRequest, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("post", postRequest);
+            return "create_post";
+        }
         postService.createPost(postRequest);
+        return "redirect:/blog/posts";
     }
 
-    @GetMapping("/admin/posts")
+    @GetMapping("/home")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/posts/create")
+    public String postForm(Model model) {
+        model.addAttribute("post", new PostRequest());
+        return "create_post";
+    }
+
+
+    @GetMapping("/posts")
     public String posts(Model model) {
         model.addAttribute("posts", postService.findAllPosts());
         return "posts";
+    }
+
+    @GetMapping("/posts/edit/{id}")
+    public String editPostForm(@PathVariable UUID id, Model model) {
+        PostResponse post = postService.getPostById(id);
+        PostRequest postRequest = PostRequest.fromResponse(post);
+        model.addAttribute("post", postRequest);
+        return "create_post";
+    }
+
+    @PostMapping("/posts/edit")
+    public String updatePost(@Valid @ModelAttribute("post") PostRequest postRequest, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("post", postRequest);
+            return "create_post";
+        }
+
+        postService.updatePost(postRequest);
+        return "redirect:/blog/posts";
+    }
+
+    @GetMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable UUID id) {
+        postService.deletePost(id);
+        return "redirect:/blog/posts";
+    }
+
+    @GetMapping("/posts/{url}")
+    public String viewPost(@PathVariable String url, Model model) {
+        PostResponse post = postService.getPostByUrl(url);
+        model.addAttribute("post", post);
+        return "view_post";
     }
 }
