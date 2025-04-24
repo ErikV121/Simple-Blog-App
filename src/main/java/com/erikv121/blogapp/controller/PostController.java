a08a629c963412086361ef1510c84a8ce9f19c42
ErikV121 <ErikVasquez121@gmail.com>
2025-04-23 21:50:52 -0400

    small fixes: Principal

--- src/main/java/com/erikv121/blogapp/controller/PostController.java
+++ src/main/java/com/erikv121/blogapp/controller/PostController.java
@@ -1,31 +1,45 @@
 package com.erikv121.blogapp.controller;
 
 import com.erikv121.blogapp.dto.request.PostRequest;
+import com.erikv121.blogapp.dto.response.CommentResponseDTO;
 import com.erikv121.blogapp.dto.response.PostResponse;
+import com.erikv121.blogapp.service.CommentService;
 import com.erikv121.blogapp.service.PostService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
 @RequestMapping("/blog")
 public class PostController {
     private final PostService postService;
+    private final CommentService commentService;
 
-    public PostController(PostService postService) {
+
+    public PostController(PostService postService, CommentService commentService) {
         this.postService = postService;
+        this.commentService = commentService;
     }
 
     @PostMapping("/posts")
-    public String createPost(@Valid @ModelAttribute("post") PostRequest postRequest, BindingResult bindingResult, Model model) {
+    public String createPost(@Valid @ModelAttribute("post") PostRequest postRequest,
+                             BindingResult bindingResult,
+                             Principal principal,
+                             Model model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("post", postRequest);
             return "create_post";
         }
-        postService.createPost(postRequest);
+
+        // Get username from authenticated user
+        String username = principal.getName();
+        postService.createPost(postRequest, username);
         return "redirect:/blog/posts";
     }
 
@@ -35,11 +49,15 @@ public class PostController {
         return "create_post";
     }
 
+
     @GetMapping("/posts")
-    public String posts(Model model) {
-        model.addAttribute("posts", postService.findAllPosts());
+    public String posts(Model model, Principal principal) {
+        String username = principal.getName();
+        System.out.println("Username: " + username);
+        model.addAttribute("posts", postService.findPostsByUser(username));
         return "posts";
     }
+
 
     @GetMapping("/posts/edit/{id}")
     public String editPostForm(@PathVariable UUID id, Model model) {
@@ -69,6 +87,9 @@ public class PostController {
     public String viewPost(@PathVariable String url, Model model) {
         PostResponse post = postService.getPostByUrl(url);
         model.addAttribute("post", post);
+
+        List<CommentResponseDTO> comments = commentService.getCommentsByPostId(post.getId());
+        model.addAttribute("comments", comments);
         return "view_post";
     }
 
