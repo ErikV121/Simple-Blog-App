f86f5cf870a659a8f353163e41325665719ca368
ErikV121 <ErikVasquez121@gmail.com>
2025-05-07 18:11:59 -0400

    Implemented KeyCloak: removed custom auth functions

--- src/main/java/com/erikv121/blogapp/entity/User.java
+++ src/main/java/com/erikv121/blogapp/entity/User.java
@@ -2,20 +2,13 @@ package com.erikv121.blogapp.entity;
 
 import com.erikv121.blogapp.entity.enums.Role;
 import jakarta.persistence.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.time.LocalDateTime;
 import java.util.*;
 
 @Entity
-@Table(name = "users") //avoiding SQL reserved word "user"
+@Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
-public class User implements UserDetails {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -25,57 +18,30 @@ public class User implements UserDetails {
     @Column(nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role = Role.User;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private Set<Comment> comments = new HashSet<>();
 
-     @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
-     private Set<Post> posts = new HashSet<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    private Set<Post> posts = new HashSet<>();
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime updatedAt;
-
-     @Column(nullable = false)
-     private boolean enabled = true;
-
-     @Column(nullable = false)
-     private boolean accountNonExpired = true;
-
-     @Column(nullable = false)
-     private boolean credentialsNonExpired = true;
-
-     @Column(nullable = false)
-     private boolean accountNonLocked = true;
-
-    public Role getRole() {
-        return role;
+    public User(String username, String email) {
+        this.username = username;
     }
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "role", nullable = false)
-    private Role role = Role.User; // Default role
 
     public User() {
-    }
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
     }
 
     public UUID getId() {
         return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -86,20 +52,12 @@ public class User implements UserDetails {
         this.username = username;
     }
 
-    public String getEmail() {
-        return email;
+    public Role getRole() {
+        return role;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public Set<Comment> getComments() {
@@ -110,57 +68,12 @@ public class User implements UserDetails {
         this.comments = comments;
     }
 
-    public void addComment(Comment comment) {
-        this.comments.add(comment);
-        comment.setUser(this);
+    public Set<Post> getPosts() {
+        return posts;
     }
 
-    public void removeComment(Comment comment) {
-        this.comments.remove(comment);
-        comment.setUser(null);
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
     }
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        // Map your roles (Set<String> or Set<Role>) to SimpleGrantedAuthority objects
-//        // Example with Set<String> roles:
-//        // return this.roles.stream()
-//        //        .map(SimpleGrantedAuthority::new)
-//        //        .collect(Collectors.toList());
-//        return Set.of(new SimpleGrantedAuthority("USER")); // Placeholder
-//    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
-    }
-
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return accountNonExpired;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return accountNonLocked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return credentialsNonExpired;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
 }
