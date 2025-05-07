f7665cf98014df0962dd2671ac6a999f4f11fa82
ErikV121 <ErikVasquez121@gmail.com>
2025-05-07 18:13:35 -0400

    Added Custom Queries

--- src/main/java/com/erikv121/blogapp/repository/PostRepository.java
+++ src/main/java/com/erikv121/blogapp/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.erikv121.blogapp.repository;
 
 import com.erikv121.blogapp.entity.Post;
+import com.erikv121.blogapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,8 +12,14 @@ import java.util.UUID;
 public interface PostRepository extends JpaRepository<Post, UUID> {
     Optional<Post> findByUrl(String url);
 
-    List<Post> findPostsByTitle(String title);
+    List<Post> findByTitleContainingIgnoreCase(String title);
+
+    List<Post> findByUser(User user);
 
     @Query("SELECT p FROM Post p ORDER BY  p.createdAt DESC")
     List<Post> findAllPostOrderDESC();
+
+    @Query("SELECT p FROM Post p ORDER BY FUNCTION('RAND')")
+    List<Post> findAllPostsRandomly();
+
 }
