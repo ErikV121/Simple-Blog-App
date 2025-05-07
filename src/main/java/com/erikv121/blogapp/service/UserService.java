5a6460870c5aa7eb73e5ae0aa3f4d14cb381ad79
ErikV121 <ErikVasquez121@gmail.com>
2025-05-07 18:05:48 -0400

    Removed register service

--- src/main/java/com/erikv121/blogapp/service/UserService.java
+++ src/main/java/com/erikv121/blogapp/service/UserService.java
@@ -1,58 +1,26 @@
 package com.erikv121.blogapp.service;
 
-import com.erikv121.blogapp.dto.request.UserRequestDto;
-import com.erikv121.blogapp.dto.response.UserResponseDto;
 import com.erikv121.blogapp.entity.User;
 import com.erikv121.blogapp.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
-    public UserResponseDto registerUser(UserRequestDto userRequestDto) {
-        // Check if passwords match
-        if (!userRequestDto.getPassword().equals(userRequestDto.getConfirmPassword())) {
-            throw new IllegalArgumentException("Passwords do not match");
+    public void createOrCheckUserExists(String username) {
+        if (userRepository.findByUsername(username).isPresent()) {
+// do nothing
+            System.out.println("User already exists");
+        } else {
+            System.out.println("User does not exist, creating new user");
+            User newUser = new User();
+            newUser.setUsername(username);
+            userRepository.save(newUser);
         }
-
-//        // Check if username already exists
-//        if (userRepository.existsByUsername(userRequestDto.getUsername())) {
-//            throw new IllegalArgumentException("Username already exists");
-//        }
-//
-//        // Check if email already exists
-//        if (userRepository.existsByEmail(userRequestDto.getEmail())) {
-//            throw new IllegalArgumentException("Email already exists");
-//        }
-
-        // Create new user
-        User user = new User();
-        user.setUsername(userRequestDto.getUsername());
-        user.setEmail(userRequestDto.getEmail());
-        user.setPassword(passwordEncoder.encode(userRequestDto.getPassword()));
-
-        // Save user
-        User savedUser = userRepository.save(user);
-
-        // Convert to response DTO
-        UserResponseDto responseDto = new UserResponseDto();
-        responseDto.setId(savedUser.getId());
-        responseDto.setUsername(savedUser.getUsername());
-        responseDto.setEmail(savedUser.getEmail());
-        responseDto.setRole(savedUser.getRole());
-        responseDto.setCreatedAt(savedUser.getCreatedAt());
-
-        return responseDto;
     }
 }
